bcce2cc3798432f833af5de19fc5ab41bc541ae2
jorge juarez <jorgejuarez217@gmail.com>
2022-06-01 14:54:16 -0300

    correcciones

--- main.js
+++ main.js
@@ -38,49 +38,7 @@ usuario.addBook('El codigo Da Vinci', 'Dan Brown');
 usuario.getBookNames(); 
  */
 
- //codigo para entrega de "manejo de archivos"
-/* 
- const fs = require('fs')
- class Contenedor {
-     constructor(fileName) {
-         this.fileName = fileName
-         fs.promises.writeFile(`./${fileName}`, '')
-     }
-     async save(objeto) {
-         let data = await fs.promises.readFile(`./${this.fileName}`, 'utf-8')
-         if(!data) {
-             objeto.id = 1
-             const arr = [objeto]
-             await fs.promises.writeFile(`./${this.fileName}`, JSON.stringify(arr))
-             return objeto.id
-         } else {
-             data = JSON.parse(data);
-             objeto.id = data.length + 1
-             data.push(objeto)
-             await fs.promises.writeFile(`./${this.fileName}`, JSON.stringify(data))
-             return objeto.id
-         }
-     }
  
-    
-    getById(){
-
-    }
-
-    getAll(){
-
-    }
-
-    deleteById(){
-
-    }
-
-    deleteAll(){
-
-    }
-
- }
- const productos = new Contenedor('productos.txt') */
 
  const fs = require('fs')
 
