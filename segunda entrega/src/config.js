8dc971090d2768fdbe1729977ba440eb27d57afb
jorge juarez <jorgejuarez217@gmail.com>
2022-07-27 23:55:09 -0300

    segunda entrega

---
+++ segunda entrega/src/config.js
@@ -0,0 +1,17 @@
+export default {
+    mongodb:{
+        conennectionString: "mongodb://localhost:27017/ecommerce",
+    },
+    firebase:{
+        type: "service_account",
+        project_id: "proyecto-back-juarez",
+        private_key_id: "48982e6561103eb98f93bce340b7b9153a0479ce",
+        private_key: "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQCeeC1s1lycxawr\n3hFFcVoH+yn0SIFzMEuGlZLxxS+maPlhzehGmjlzrQrPtE14DpG+vuJOzJBBFIsV\n/kHOzaxenc67kjNESBbpXmud0DN5RN9EuEGPoq+WtT+A6xgzsw3bsurQg8nWpMJt\nn80eDWqJIDnm1fsbdKB414BI6riKItzx46s686PI64a1vuoI3rheZDapKdTdfMzP\ndv0d9iVsKSsB90q7Y4Io3ugSTwiNqBieGXRA3B0D4D+SXoGAkWUN8LQHKnufiWkq\nNujGXCgFZAuvP8J9lqAQXUril5F4O9v2IGMWG6zoUB3ckx97zVqrq7jJvpHRch3Q\nrgHRuPcNAgMBAAECggEAG68Q+HeGXFjoGA3GZKRFvykwjh1wh+gC+bL9pcD17xNd\npalsD5F3we6T+e8crYGWEoWUUYHJHaVyGth9NJyXpTvaVshWrnINlp1JHZu2slPG\n0WGDCo24MRLe0i2/36BDJoNeJCkxxI6p7K7DDaH4X5ZCp0HwiT5ZnmK/ZX2j2XWw\n9XdDdxnlehBmfRSQP211ax/ABdn9pXE0zcEr3PQpMQBBhh74zS8h1sCvCIqZoUew\nlNoMaoZ0rXQWUkzPpUq0+JsAkxvVi/djHSj57mt5IcKF0uD+K0ZrRRpAmoleO3lQ\nNdW8Wb/5/QhRmlGDLVtdSQzrI7Y//p/cAqm+RdBuQQKBgQDfXAvRTbO6m+VbVd9F\nhrY6alzGBPuzlc8BIRTP08izv0MLbRQA1h2I9MlreVlcEI9N1SnWBoG5H2jQi+j0\n39PEQpIcALbPNuFWfJjmpUqBPhD6DQ1juBj5JscxKhwe8rePs4mhXM8lzw1XLNqZ\n9sOb+YBD8XPKfpiuAigV2YP6cQKBgQC1oJIt0D2YRzXfleqopS44OLzX+ddW7zQb\nWdGEHGfvRJ/EWB/i1bCboRmJ5Y0MIAyyzYcw1HBdlsVL3dmzfMFsZdsF/UZsqjH8\n4yUmekx9oYV6Srp5gg/rTNSxWUYZ+rdDcmsPoz6gKgBrcXfHN2RVWT1F62D9GjE6\n+wwmhJy8XQKBgQCbQS3ttKcDRS2tyQjDGRo9jIUkF36lm7IhxM7kjv9pAb8miBFp\na2toMU54aAcXGWOi5FKKNEejWBPRMg8hdxuJxgQtdsmbSzhil4TofWhGYdAHH195\nOxQQy0WWBQE6+ddiCZfXE93hGATb3Y+tiXr6RflEQOcNj+Wsvk8CsQkt4QKBgBxs\nVHP28qyx+CxVObPKpcEGoAbsglaIeI8PL0qNZ3Z6NsZzRAorhsfUHjsGQVx0Z4hT\nwaRSmoMK5TVKMpd0HpDoH66ARFUdDVWV9mpfGTlP3aOVrjj1sagRDffzEJVgNu3H\nKhn4YgSOjiJCT4xNjtYMcRpQC42QW8PvJaeheSXBAoGBAIJIKHKGR55xZVyUg0kc\nzfRUeZWy4jsx1EJJ9scXm+Geixr0RFOATt6I1tG6gSMjHkMWwNGRn8erl1Vw4iyv\nTT+zyVGvpwc8nqgvT4wA6AawcK0eZ7Np8Tv3A26wYlpTIeotMuurZSo8lZ5ulj7l\np6juA4npyEWJ18Xj2eo5mRGN\n-----END PRIVATE KEY-----\n",
+        client_email: "firebase-adminsdk-wq07l@proyecto-back-juarez.iam.gserviceaccount.com",
+        client_id: "111031270246860899081",
+        auth_uri: "https://accounts.google.com/o/oauth2/auth",
+        token_uri: "https://oauth2.googleapis.com/token",
+        auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+        client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-wq07l%40proyecto-back-juarez.iam.gserviceaccount.com"
+      }
+}
